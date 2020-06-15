9f8a0ca09d155cb5e88e16a70629bb1e1146ba8c
İbrahim Turan <ibrahimturan002@gmail.com>
2020-06-15 03:33:00 +0300

    the latest build of frontend has integrated to project

---
+++ public/_nuxt/3ac0b4154f520c9aa00a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{444:function(n,l,o){"use strict";o.r(l);var e=o(432).default,t=o(49),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
