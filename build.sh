d8c1305ecf5121d2990181cc1d6b5a78b253bd9d
İbrahim Turan <ibrahimturan002@gmail.com>
2020-06-15 21:08:28 +0300

    bash file updated

--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@ cd public
 FILE="../public.list"
 BUILD="$HOME/Documents/Dev/Vue/ibrahimturan/dist/*"
 
+echo =============================
+echo "| UNDELETED FILES           |"
+echo -----------------------------
 for f in *; do
    if [ ! -z $(grep "$f" "$FILE") ];
    then echo $f;
    else rm -rf $f
    fi
 done
+echo ==============================
 
 cp -r $BUILD ./
 
@@ -18,6 +22,7 @@ cd ..
 git add .
 git commit  -m "latest build received"
 
-echo "success"
+echo "DONE"
+echo ==============================
 
 exec bash
