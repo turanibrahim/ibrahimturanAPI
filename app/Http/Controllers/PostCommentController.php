ac27c05a18801a0e2eda0c58d153c06a07d346a7
İbrahim Turan <ibrahimturan002@gmail.com>
2020-06-18 03:05:36 +0300

    post comments controller created and index mehtod added

---
+++ app/Http/Controllers/PostCommentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PostComment;
+use App\Http\Resources\PostCommentResource;
+
+class PostCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        return PostCommentResource::collection(PostComment::
+            where('post_id', '=' , $id)->with('parentComment:id,name,surname,comment')->paginate(12)
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
