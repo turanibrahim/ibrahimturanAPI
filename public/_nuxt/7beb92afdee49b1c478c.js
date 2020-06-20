7ccc25e2feb621748c8915c4d91cd22afe756ccc
İbrahim Turan <ibrahimturan002@gmail.com>
2020-06-20 23:00:02 +0300

    latest build received

---
+++ public/_nuxt/7beb92afdee49b1c478c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{462:function(n,l,o){"use strict";o.r(l);var e=o(449).default,t=o(51),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
